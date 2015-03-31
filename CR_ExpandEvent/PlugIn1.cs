9f6af991feed2e90b63de25d59f9b643443dd9d8
Rory Becker <RoryBecker@gmail.com>
2015-03-31 21:26:55 +0100

    - Refactored for ease of further development.

--- CR_ExpandEvent/PlugIn1.cs
+++ CR_ExpandEvent/PlugIn1.cs
@@ -42,9 +42,11 @@ namespace CR_ExpandEvent
 			ExpandEvent.ProviderName = "Expand Event"; // Should be Unique
 			ExpandEvent.DisplayName = "Expand Event";
 			ExpandEvent.CheckAvailability += ExpandEvent_CheckAvailability;
+			ExpandEvent.PreparePreview += ExpandEvent_PreparePreview;
 			ExpandEvent.Apply += ExpandEvent_Execute;
 			((System.ComponentModel.ISupportInitialize)(ExpandEvent)).EndInit();
 		}
+
 		private void ExpandEvent_CheckAvailability(Object sender, CheckContentAvailabilityEventArgs ea)
 		{
 			_EventToExpand = GetEvent(ea.Caret);
@@ -56,33 +58,20 @@ namespace CR_ExpandEvent
 				return;
 			ea.Available = true;
 		}
+		private void ExpandEvent_PreparePreview(object sender, PrepareContentPreviewEventArgs ea)
+		{
+			//ea.AddStrikethrough(_EventToExpand.NameRange);
+			//ea.AddCodePreview(_EventToExpand.Range.Start, BuildNewExpandedEventCode("_" + _EventToExpand.Name));
+		}
 		private void ExpandEvent_Execute(Object sender, ApplyContentEventArgs ea)
 		{
-			ElementBuilder Builder = ea.NewElementBuilder();
-			Variable EventField = Builder.BuildVariable("EventHandler", "_" + _EventToExpand.Name);
-
-			// Build Event and Add/Remove
-			Event NewEvent = Builder.BuildEvent(_EventToExpand.Name, _EventToExpand.MemberTypeReference);
-			NewEvent.Visibility = _EventToExpand.Visibility;
-			EventAdd NewEventAdd = Builder.AddEventAdd(NewEvent);
-			Builder.AddAssignment(NewEventAdd, EventField.Name, "value", AssignmentOperatorType.PlusEquals);
-			EventRemove NewEventRemove = Builder.AddEventRemove(NewEvent);
-			Builder.AddAssignment(NewEventRemove, EventField.Name, "value", AssignmentOperatorType.MinusEquals);
-
 			TextDocument ActiveDoc = CodeRush.Documents.ActiveTextDocument;
-			ActiveDoc.QueueReplace(_EventToExpand, CodeRush.CodeMod.GenerateCode(NewEvent));
-			ActiveDoc.QueueInsert(_EventToExpand.Range.Start, CodeRush.CodeMod.GenerateCode(EventField));
+			ActiveDoc.ParseIfTextChanged();
+			ActiveDoc.QueueInsert(_EventToExpand.Range.Start, BuildNewEventField(_EventToExpand));
+			ActiveDoc.QueueReplace(_EventToExpand, BuildNewExpandedEventCode("_" + _EventToExpand.Name));
 			ActiveDoc.ApplyQueuedEdits("Expanded Event", true);
-			
+			ActiveDoc.ParseIfTextChanged();
 		}
-		private Event GetEvent(SourcePoint Caret)
-		{
-			LanguageElement Element = CodeRush.Documents.ActiveTextDocument.GetNodeAt(Caret);
-			if (Element == null)
-				return null;
-			return Element.GetParentEvent();
-		}
-
 		public void registerCompressEvent()
 		{
 			DevExpress.Refactor.Core.RefactoringProvider CompressEvent = new DevExpress.Refactor.Core.RefactoringProvider(components);
@@ -90,12 +79,14 @@ namespace CR_ExpandEvent
 			CompressEvent.ProviderName = "Compress Event"; // Should be Unique
 			CompressEvent.DisplayName = "Compress Event";
 			CompressEvent.CheckAvailability += CompressEvent_CheckAvailability;
+			CompressEvent.PreparePreview += CompressEvent_PreparePreview;
 			CompressEvent.Apply += CompressEvent_Execute;
 			((System.ComponentModel.ISupportInitialize)(CompressEvent)).EndInit();
 		}
 		private void CompressEvent_CheckAvailability(Object sender, CheckContentAvailabilityEventArgs ea)
 		{
-			CodeRush.Documents.ActiveTextDocument.ParseIfNeeded();
+			TextDocument ActiveDoc = CodeRush.Documents.ActiveTextDocument;
+			ActiveDoc.ParseIfTextChanged();
 			_EventToCompress = GetEvent(ea.Caret);
 			// Exit if no Event found under caret
 			if (_EventToCompress == null)
@@ -120,7 +111,6 @@ namespace CR_ExpandEvent
 			if (EventAddAssign.LeftSide.Name != EventRemoveAssign.LeftSide.Name)
 				return;
 
-
 			var EventField = EventAddAssign.LeftSide;
 			_EventToCompressFieldDeclaration = (Variable)EventField.GetDeclaration().ToLanguageElement();
 			// Variable must be of type EventHandler
@@ -133,36 +123,62 @@ namespace CR_ExpandEvent
 
 			ea.Available = true;
 		}
+		private void CompressEvent_PreparePreview(object sender, PrepareContentPreviewEventArgs ea)
+		{
+			//ea.AddStrikethrough(_EventToCompress.Range);
+			//ea.AddCodePreview(_EventToCompress.Range.Start, BuildNewCompressedEventCode(_EventToCompress));
+		}
 		private void CompressEvent_Execute(Object sender, ApplyContentEventArgs ea)
 		{
-			ElementBuilder Builder = ea.NewElementBuilder();
-
-			// Build Event and Add/Remove
-			Event NewEvent = Builder.BuildEvent(_EventToCompress.Name, _EventToCompress.MemberTypeReference);
-			NewEvent.Visibility = _EventToCompress.Visibility;
-
 			TextDocument ActiveDoc = CodeRush.Documents.ActiveTextDocument;
-			ActiveDoc.QueueDelete(_EventToCompressFieldDeclaration);
-			ActiveDoc.QueueReplace(_EventToCompress, CodeRush.CodeMod.GenerateCode(NewEvent, true));
+			ActiveDoc.ParseIfTextChanged();
+
+			ActiveDoc.QueueDelete(_EventToCompress);
+			ActiveDoc.QueueReplace(_EventToCompressFieldDeclaration, BuildNewCompressedEventCode(_EventToCompress));
 			ActiveDoc.ApplyQueuedEdits("Compressed Event");
+			ActiveDoc.ParseIfTextChanged();
+
 		}
-		
 
-
-		// Example code
-		public event EventHandler MyEvent;
-
-		private EventHandler _myEvent2;
-		public event EventHandler MyEvent2
+		private string BuildNewEventField(Event SourceEvent)
 		{
-			add
-			{
-				_myEvent2 += value;
-			}
-			remove
-			{
-				_myEvent2 -= value;
-			}
+			ElementBuilder Builder = new ElementBuilder();
+			Variable EventField = Builder.BuildVariable(SourceEvent.MemberType , "_" + SourceEvent.Name);
+			EventField.Visibility = _EventToExpand.Visibility;
+			return GenerateCode(EventField);
+		}
+		private string BuildNewExpandedEventCode(string FieldName)
+		{
+			ElementBuilder Builder = new ElementBuilder();
+			Event NewEvent = Builder.BuildEvent(_EventToExpand.Name, _EventToExpand.MemberTypeReference);
+			NewEvent.Visibility = _EventToExpand.Visibility;
+
+			EventAdd NewEventAdd = Builder.AddEventAdd(NewEvent);
+			Builder.AddAssignment(NewEventAdd, FieldName, "value", AssignmentOperatorType.PlusEquals);
+			EventRemove NewEventRemove = Builder.AddEventRemove(NewEvent);
+			Builder.AddAssignment(NewEventRemove, FieldName, "value", AssignmentOperatorType.MinusEquals);
+
+			return GenerateCode(NewEvent);
+		}
+
+		private string BuildNewCompressedEventCode(Event SourceEvent)
+		{
+			ElementBuilder Builder = new ElementBuilder();
+			Event NewEvent = Builder.BuildEvent(SourceEvent.Name, SourceEvent.MemberType);
+			NewEvent.Visibility = SourceEvent.Visibility;
+			return GenerateCode(NewEvent);
+		}
+
+		private Event GetEvent(SourcePoint Caret)
+		{
+			LanguageElement Element = CodeRush.Documents.ActiveTextDocument.GetNodeAt(Caret);
+			if (Element == null)
+				return null;
+			return Element.GetParentEvent();
+		}
+		private static string GenerateCode(LanguageElement Element)
+		{
+			return CodeRush.CodeMod.GenerateCode(Element, true);
 		}
 	}
 }
